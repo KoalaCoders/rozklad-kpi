35cd4697775ae8ed6f24c43c4c4c605c8413cbe5
Ihor Motornyi <igormotorny1997@gmail.com>
2016-11-12 13:27:37 +0200

    Added some code

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -6,8 +6,13 @@ import 'rxjs/add/operator/map';
 export class ApiService {
   thimetable: any;
   timetableObserver: any;
-
   constructor(private http: Http) { }
+
+
+  getGroups() {
+    return this.http.get('http://api.rozklad.org.ua/v2/groups')
+      .map(res => res.json().data);
+  }
 
   searchGroup(query: string) {
     return this.http
