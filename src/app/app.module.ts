f74fe258b9d721af0d194443c2813e2e209b956d
Ihor Motornyi <igormotorny1997@gmail.com>
2016-11-12 23:01:58 +0200

    Class work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,12 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './components/app/app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { SearchComponent } from './components/search/search.component';
-import { TimetableComponent } from './components/timetable/timetable.component';
-import { MapToItarablePipe } from './pipes/map-to-itarable/map-to-itarable.pipe';
-import { LessonComponent } from './components/lesson/lesson.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SearchComponent,
-    TimetableComponent,
-    MapToItarablePipe,
-    LessonComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-import { ApiService } from '../../services/api/api.service';
+import { Component, OnInit } from '@angular/core';
 import 'rxjs/add/operator/map';
+import { ApiService } from '../../services/api/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [ApiService],
+  providers: [ApiService]
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  data: Array<Object>;
+
+  constructor(private api: ApiService) { }
+  ngOnInit():void {
+    this.api.getGroups()
+      .subscribe((data) => {
+        console.log(data);
+        this.data = data;
+      });
+  }
+}
